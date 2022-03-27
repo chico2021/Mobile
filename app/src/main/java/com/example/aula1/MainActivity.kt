d2af12f24486e3f983b52c7e0d0ddf60f462b1a2
pool <tofikad538@ofenbuy.com>
2022-03-27 10:57:04 -0300

    start commit!!

---
+++ app/src/main/java/com/example/aula1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.aula1
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+
+
+
+
+  }
+
+
+
+
+
+
